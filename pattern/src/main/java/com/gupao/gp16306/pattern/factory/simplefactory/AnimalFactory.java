f486a5b527b95778ca3f85c35ca960d5563e02e3
fire_feng <369596957@qq.com>
2019-03-12 14:56:53 +0800

    简单工厂模式 示例

--- pattern/src/main/java/com/gupao/gp16306/pattern/factory/simplefactory/AnimalFactory.java
+++ pattern/src/main/java/com/gupao/gp16306/pattern/factory/simplefactory/AnimalFactory.java
@@ -14,4 +14,26 @@ public class AnimalFactory {
         }else
             return null;
     }
+
+    public IAnimal createAnimal2(String className){
+        try {
+            if (!("".equals(className) || null == className)) {
+                return  (IAnimal) Class.forName(className).newInstance();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public  IAnimal createAnimal3(Class<? extends IAnimal> clazz){
+        try {
+        if ( null != clazz){
+                return clazz.newInstance();
+        }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- pattern/src/main/java/com/gupao/gp16306/pattern/factory/simplefactory/AnimalFactoryTest.java
+++ pattern/src/main/java/com/gupao/gp16306/pattern/factory/simplefactory/AnimalFactoryTest.java
@@ -1,6 +1,7 @@
 package com.gupao.gp16306.pattern.factory.simplefactory;
 
 import com.gupao.gp16306.pattern.factory.Cat;
+import com.gupao.gp16306.pattern.factory.Dog;
 import com.gupao.gp16306.pattern.factory.IAnimal;
 
 public class AnimalFactoryTest {
@@ -9,6 +10,15 @@ public class AnimalFactoryTest {
         animal.say();
         animal.eat();
         animal = new AnimalFactory().ceateAnimal("dog");
+        animal.say();
+        animal.eat();
+
+        animal = new AnimalFactory().createAnimal2("com.gupao.gp16306.pattern.factory.Cat");
+        animal.say();
+        animal.eat();
+
+        animal = new AnimalFactory().createAnimal3(Dog.class);
+
         animal.say();
         animal.eat();
     }

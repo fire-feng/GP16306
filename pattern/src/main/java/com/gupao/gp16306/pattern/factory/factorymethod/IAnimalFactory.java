700822719bf631c775efd92f451ee1f3839fb874
fire_feng <369596957@qq.com>
2019-03-12 21:58:19 +0800

    .gitignore commit

---
+++ pattern/src/main/java/com/gupao/gp16306/pattern/factory/factorymethod/IAnimalFactory.java
@@ -0,0 +1,9 @@
+package com.gupao.gp16306.pattern.factory.factorymethod;
+
+import com.gupao.gp16306.pattern.factory.IAnimal;
+
+public interface IAnimalFactory {
+
+    IAnimal create();
+
+}
--- pattern/src/main/java/com/gupao/gp16306/pattern/factory/simplefactory/AnimalFactoryTest.java
+++ pattern/src/main/java/com/gupao/gp16306/pattern/factory/simplefactory/AnimalFactoryTest.java
@@ -6,18 +6,19 @@ import com.gupao.gp16306.pattern.factory.IAnimal;
 
 public class AnimalFactoryTest {
     public static void main(String[] args) {
+        AnimalFactory factory = new AnimalFactory();
         IAnimal animal = new Cat();
         animal.say();
         animal.eat();
-        animal = new AnimalFactory().ceateAnimal("dog");
+        animal = factory.ceateAnimal("dog");
         animal.say();
         animal.eat();
 
-        animal = new AnimalFactory().createAnimal2("com.gupao.gp16306.pattern.factory.Cat");
+        animal = factory.createAnimal2("com.gupao.gp16306.pattern.factory.Cat");
         animal.say();
         animal.eat();
 
-        animal = new AnimalFactory().createAnimal3(Dog.class);
+        animal = factory.createAnimal3(Dog.class);
 
         animal.say();
         animal.eat();
